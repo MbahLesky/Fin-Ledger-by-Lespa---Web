81fab4d6c6421ac685ddc4d8425d334d23f0a70d
Mbah Lesky <mbahlesky4@gmail.com>
2026-04-17 23:42:14 +0100

    feat: update branding assets and references to app logo

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "finance-ledger-shell-v2";
+const CACHE_NAME = "finance-ledger-shell-v3";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/offline.html",
   "/manifest.webmanifest",
-  "/icons/icon-192.svg",
-  "/icons/icon-512.svg"
+  "/icons/fin_track_logo.png",
+  "/icons/icon.svg"
 ];
 
 self.addEventListener("install", (event) => {
